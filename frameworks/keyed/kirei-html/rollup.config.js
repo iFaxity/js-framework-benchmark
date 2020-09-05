24729ea358d5b0fc80f45c03dc92b5c3994115f2
iFaxity <izze96@gmail.com>
2020-09-05 18:44:02 +0200

    Upgraded framework versions

--- frameworks/keyed/kirei-html/rollup.config.js
+++ frameworks/keyed/kirei-html/rollup.config.js
@@ -1,10 +1,12 @@
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import globals from 'rollup-plugin-node-globals';
 
 export default {
   input: 'src/index.js',
   plugins: [
+    globals(),
     minifyHTML({
       options: {
         minifyOptions: {
--- frameworks/non-keyed/kirei-html/rollup.config.js
+++ frameworks/non-keyed/kirei-html/rollup.config.js
@@ -1,19 +1,21 @@
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import globals from 'rollup-plugin-node-globals';
 
 export default {
   input: 'src/index.js',
   plugins: [
+    globals(),
     minifyHTML({
       options: {
         minifyOptions: {
           keepClosingSlash: true
-        }
-      }
+        },
+      },
     }),
     resolve(),
-    terser()
+    terser(),
   ],
   context: 'null',
   moduleContext: 'null',

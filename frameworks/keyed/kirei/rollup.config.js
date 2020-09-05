3f4c293f7540694f707a5acc0d8d92b39740c74a
iFaxity <izze96@gmail.com>
2020-09-06 01:06:41 +0200

    Updated package dependencies

--- frameworks/keyed/kirei/rollup.config.js
+++ frameworks/keyed/kirei/rollup.config.js
@@ -1,13 +1,11 @@
 import { terser } from 'rollup-plugin-terser';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 import resolve from '@rollup/plugin-node-resolve';
-import globals from 'rollup-plugin-node-globals';
 
 export default {
   input: `src/main.js`,
   output: { file: `dist/main.js`, format: 'iife', name: 'litFx' },
   plugins: [
-    globals(),
     resolve(),
     minifyHTML(),
     terser({ warnings: true })
